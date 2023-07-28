3a539bd2249658647fe9a914f58b36be5b4f58d0
ismail hosen <ismailwdpf@gmail.com>
2023-07-29 00:21:55 +0600

    frontend desing

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('frontend.home');
+        $allproducts = Product::latest()->get();
+        return view('frontend.home',compact('allproducts'));
     }
 }
