dd1c851fdfa3480b7f0d8043bb2c970a6e65f6a1
ismail hosen <ismailwdpf@gmail.com>
2023-08-08 17:23:39 +0600

    cart product total price

--- resources/views/frontend/layouts/addtocart.blade.php
+++ resources/views/frontend/layouts/addtocart.blade.php
@@ -12,24 +12,37 @@
             <div class="table-responsive">
                 <table class="table">
                     <tr>
-                        <th>Product Id</th>
+                        <th>Product Name</th>
+                        <th>Image</th>
                         <th>Quantity</th>
                         <th>Price</th>
                         <th>Action</th>
                     </tr>
+                    @php
+                        $total = 0;
+                    @endphp
                     @foreach ($cart_items as $item)
                   
                     <tr>
                         @php
                         $product_name = App\Models\Product::where('id', $item->product_id)->value('product_name');
-                    @endphp
+                        $image = App\Models\Product::where('id', $item->product_id)->value('product_img');
+                        @endphp
                         <td>{{ $product_name }}</td>
+                        <td><img style="height:80px" src="{{asset($image)}}" alt="img"></td>
+
                         <td>{{ $item->quantity }}</td>
                         <td>{{ $item->price }}</td>
                         <td><a href="" class="btn btn-danger">Remove</a></td>
                     </tr>
-                        
+                        @php
+                            $total = $total + $item->price;                      
+                        @endphp                       
                     @endforeach
+                    <tr>
+                        <td>Total</td>
+                        <td>{{ $total }}</td>
+                    </tr>
                 </table>
             </div>
         </div>
